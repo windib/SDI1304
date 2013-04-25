764d6009494b43e0156bf053b7946dc69784c63e
Windi Brunelle <windibahl@fullsail.edu>
2013-04-25 11:25:51 -0400

    adding first files to the repo

---
+++ bahl_windi_assignment3.js
@@ -0,0 +1,57 @@
+// MyName:  Windi Bahl
+// Date:  Apr 25, 2013
+// Assignment:  Project 3
+
+//declare variables
+var 	seeds = 	["squash ", "green beans", "butter beans", "cucumbers"];
+ 	row = 		28
+	
+
+
+//json
+
+var typesOfSeeds = {
+	"garden": [
+		{
+			"seed1": 	"Squash",
+			"rows": 	"8",
+			"numberSeeds": 	"200",
+			"seedPerRow":	"25",
+			"pckRows":	["Windi", " Grandma", " Wanda"]
+			
+		},
+		{
+			"seed1": 	"Green Bean",
+			"rows": 	"4",
+			"numberSeeds": 	"120",
+			"seedPerRow":	"30",
+			"pckRows":	["Wanda", " Richard", " Sybil"]
+		},
+		{
+			"seed1": 	"Butter Beans",
+			"rows": 	"6",
+			"numberSeeds": 	"300",
+			"seedPerRow":	"50",
+			"pckRows":	["Windi", " Debbie", " Michael"]
+		},
+		{
+			"seed1": 	"Squash",
+			"rows": 	"10",
+			"numberSeeds": 	"400",
+			"seedPerRow":	"40",
+			"pckRows":	["Grandma", " Sybil", " Richard"]
+		},
+	]
+	
+};
+
+//string function
+var getLoot = function(seed1,seed2,seed3,seed4){
+	var	seed1 = 	
+	var	seed2 =		
+		loot = seed1 + " and " + seed2
+	
+return loot;
+};
+
+
